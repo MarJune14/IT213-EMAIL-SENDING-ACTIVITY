9e4bfe038e1f5f2803f9c4925518ad351cf6d0a1
MarJune14 <gereromarjune14@gmail.com>
2021-10-26 20:36:08 -0700

    Validation.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 //variables
-const sendBtn = document.getElementById('sendBtn');
+const sendBtn = document.getElementById('sendBtn'),
+       email = document.getElementById('email'),
+       subject = document.getElementById('subject'),
+       message = document.getElementById('message');
 
 
 //EventListeners
@@ -10,6 +13,10 @@ function eventListeners() {
     //app init
     document.addEventListener('DOMContentLoaded', appInit);
 
+    //Validate the Forms
+    email.addEventListener('blur', validateField);
+    subject.addEventListener('blur', validateField);
+    message.addEventListener('blur', validateField);
 }
 
 //functions
@@ -19,4 +26,49 @@ function appInit() {
     //disabled send button on load
     sendBtn.disabled = true; 
 
+}
+
+//validate fields
+
+function validateField() {
+    let errors;
+
+
+    //validate the lenght of fields
+    validateLength(this)
+
+    //validate the email
+    if(this.type === 'email') {
+        validateEmail(this);
+    }
+}
+
+//validate the length of the field
+
+function validateLength(field) {
+    if(field.value.length > 0 ) {
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    } else {
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+
+    }
+}
+
+//validate email
+
+function validateEmail(field) {
+    let emailText = field.value;
+
+    //chech if the emailText contains the @ sign
+    if(emailText.indexOf('@') !== -1) {
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    } else {
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+
+    }
+     
 }
