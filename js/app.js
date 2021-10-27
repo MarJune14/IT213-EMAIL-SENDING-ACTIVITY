b5ee9aea185d12eb9ffc5c743ceef79c2e3cd36b
MarJune14 <gereromarjune14@gmail.com>
2021-10-26 22:17:51 -0700

    Sending Emails.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@
 const sendBtn = document.getElementById('sendBtn'),
        email = document.getElementById('email'),
        subject = document.getElementById('subject'),
-       message = document.getElementById('message');
+       message = document.getElementById('message'),
+       resetBtn = document.getElementById('resetBtn'),
+       sendEmailForm = document.getElementById('email-form');
 
 
 //EventListeners
@@ -17,6 +19,11 @@ function eventListeners() {
     email.addEventListener('blur', validateField);
     subject.addEventListener('blur', validateField);
     message.addEventListener('blur', validateField);
+
+    //send email and reset button
+    sendEmailForm.addEventListener('submit', sendEmail);
+
+    resetBtn.addEventListener('click', resetForm);
 }
 
 //functions
@@ -25,6 +32,37 @@ function eventListeners() {
 function appInit() {
     //disabled send button on load
     sendBtn.disabled = true; 
+
+}
+function sendEmail(e) {
+    e.preventDefault();
+
+
+    //show the spinner
+    const spinner = document.querySelector('#spinner');
+    spinner.style.display = 'block';
+
+
+    //show the image
+    const sendEmailImg = document.createElement('Img');
+    sendEmailImg.src = 'img/mail.gif';
+    sendEmailImg.style.display = 'block';
+
+    //hide the spinner and show the send email image
+    setTimeout(function() {
+        //hide the spinner
+        spinner.style.display = 'none';
+
+        //show the image
+        document.querySelector('#loaders').appendChild( sendEmailImg );
+
+        //hide the image
+        setTimeout(function() {
+            sendEmailForm.reset();
+            sendEmailImg.remove();
+        }, 5000);
+
+    }, 3000);
 
 }
 
@@ -40,6 +78,18 @@ function validateField() {
     //validate the email
     if(this.type === 'email') {
         validateEmail(this);
+    }
+
+    //both will return errors, then check if there're any errors
+    errors = document.querySelectorAll('.error');
+
+    //check that the inputs are not empty
+    if(email.value !== '' && subject.value !== '' && message.value !== '' ) {
+        if(errors.length === 0 ) {
+            //the button should be enabled
+            sendBtn.disabled = false;
+        }
+
     }
 }
 
@@ -71,4 +121,9 @@ function validateEmail(field) {
 
     }
      
+}
+
+//rset the form
+function resetForm() {
+    sendEmailForm.reset();
 }
